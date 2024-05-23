f4ba04de9beb2c6f072f53c875a47e39bdf40b4a
Thomas <software@thomasdelage.com>
2024-05-23 15:07:04 +0200

    view error fix

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,4 +5,5 @@ return [
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\VoltServiceProvider::class,
     App\Providers\CookiesServiceProvider::class,
+    Illuminate\View\ViewServiceProvider::class,
 ];
